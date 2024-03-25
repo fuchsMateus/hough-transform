4c6e89d88a44632c4c5726f4057cbbc664c851ae
Mateus Fuchs <mateus5fuchs@gmail.com>
2024-03-25 13:06:06 -0300

    Licença adicionada

--- preprocessar.js
+++ preprocessar.js
@@ -3,8 +3,9 @@ export function escalaCinza(ctx, w, h) {
     const data = imageData.data;
 
     for (let i = 0; i < data.length; i += 4) {
-        //ITU BT.601
-        const cinza = 0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
+        //ITU BT.709
+        //Normalizar os valores sRGB para 0-1, aplicar o expoente 2.2, multiplicar pelo coeficiente, somar, aplicar o expoente reverso, reverter a normalização.
+        let cinza = Math.min(255,((data[i]/255.0)**2.2*0.2126+(data[i + 1]/255.0)**2.2*0.7152+(data[i + 2]/255.0)**2.2*0.0722)**0.4545*255);
         data[i] = data[i + 1] = data[i + 2] = cinza;
        
     }
@@ -43,7 +44,7 @@ export function suavizacaoGaussiana(ctx, w ,h){
         for (let x = 0; x < w; x++) {
             let px = (y * w + x) * 4;
             if(y<dViz || y >= h-dViz || x<dViz || x >= w-dViz){
-                gaussData[px] = gaussData[px + 1] = gaussData[px + 2] = imageData.data[px]; //R, G e 
+                gaussData[px] = gaussData[px + 1] = gaussData[px + 2] = imageData.data[px];
                 gaussData[px + 3] = 255; 
                 continue;
             }
@@ -94,9 +95,10 @@ export function filtroDeSobel(ctx, w ,h) {
 
             let magnitude = Math.round(Math.sqrt(gx * gx + gy * gy));
 
-            sobelData[px] = sobelData[px + 1] = sobelData[px + 2] = magnitude; //R, G e B
-            sobelData[px + 3] = 255; //Alpha
+            sobelData[px] = sobelData[px + 1] = sobelData[px + 2] = magnitude; 
+            sobelData[px + 3] = 255; 
         }
     }
     ctx.putImageData(new ImageData(sobelData, w, h), 0, 0);
 }
+
